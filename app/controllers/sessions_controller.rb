07627f33e7f031c3be7380d75d1b7798a8f0c77d
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-21 16:42:34 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
     end
   
     def create
+      binding.pry
       user = User.find_by(name: params[:user][:name])
       user = user.try(:authenticate, params[:user][:password])
       return redirect_to(controller: 'sessions', action: 'new') unless user
       session[:user_id] = user.id
       @user = user
-      
+
       redirect_to controller: 'welcome', action: 'home'
     end
   
